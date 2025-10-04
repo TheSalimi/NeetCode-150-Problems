c58239a7292660315e1459a60dcc9b85ce3e38a9
Amir Hossein Salimi <108394058+TheSalimi@users.noreply.github.com>
2025-10-04 11:38:08 +0330

    Update Group-Anagrams.cs

--- Array & Hashing/Group-Anagrams.cs
+++ Array & Hashing/Group-Anagrams.cs
@@ -1,3 +1,9 @@
+/*
+Given an array of strings strs, group the anagrams together. An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all the original letters exactly once.
+
+Return the result as a list of lists, where each inner list contains strings that are anagrams of each other. The order of the output does not matter.
+*/
+
 public static IList<IList<string>> GroupAnagrams(string[] strs)
 {
     Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
